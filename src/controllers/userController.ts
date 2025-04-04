87eaf5da3ed8f85b2a32e4b6db070e0b7dd901eb
RandikaEdirisooriya <randikaedirisooriya@gmail.com>
2025-04-04 09:22:23 +0530

    making all crud in users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,3 +9,39 @@ export const createUser = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Create user failed", details: err });
   }
 };
+
+export const listUsers = async (_: Request, res: Response) => {
+  try {
+    const users = await Weavy.listUsers();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: "List users failed" });
+  }
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const user = await Weavy.getUser(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: "Get user failed" });
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await Weavy.updateUser(req.params.id, req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: "Update failed" });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const response = await Weavy.deleteUser(req.params.id);
+    res.json(response);
+  } catch (err) {
+    res.status(500).json({ error: "Delete failed" });
+  }
+};
--- src/services/weavyServices.ts
+++ src/services/weavyServices.ts
@@ -10,3 +10,23 @@ export const createUser = async (data: any) => {
   const res = await axios.post(`${WEAVY_BASE_URL}/api/users`, data, { headers });
   return res.data;
 };
+
+export const listUsers = async () => {
+  const res = await axios.get(`${WEAVY_BASE_URL}/api/users`, { headers });
+  return res.data;
+};
+
+export const getUser = async (id: string) => {
+  const res = await axios.get(`${WEAVY_BASE_URL}/api/users/${id}`, { headers });
+  return res.data;
+};
+
+export const updateUser = async (id: string, data: any) => {
+  const res = await axios.put(`${WEAVY_BASE_URL}/api/users/${id}`, data, { headers });
+  return res.data;
+};
+
+export const deleteUser = async (id: string) => {
+  const res = await axios.delete(`${WEAVY_BASE_URL}/api/users/${id}`, { headers });
+  return res.data;
+};
